942baea144d28e8cc1f10e978ae789ce7907e5e8
AnglyPascal <ahsanalmahir@gmail.com>
2021-01-05 23:46:23 +0600

    new theme

---
+++ docs/_posts/2020-01-05-Hello_World.md
@@ -0,0 +1,8 @@
+---
+layout : single
+title  : "Hello World"
+---
+
+# Let's go...
+
+Hello, this is my first blog post :D
---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Let’s go…   Hello, this is my first blog post :D  ","categories": [],
+        "tags": [],
+        "url": "/Hello_World/",
+        "teaser": null
+      }]
